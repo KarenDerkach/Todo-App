873aa9939811505a5cfe1b06f2a2b4ce15d890f2
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-03 17:32:09 -0300

    add route api

--- pages/api/tasks/list.js
+++ pages/api/tasks/list.js
@@ -1,23 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { dbConnect } from '../../../config/dbconnection'
+const dbConnect = require('../../../config/dbconnection')
+const  TasksModel = require('../../../config/TasksModel')
 
 dbConnect()
 
-import TaskModel from '../../../config/model'
 
-//import { tasks } from './_tasks'
+export default   async (req, res)=>{
+    const {method, body} = req
 
-
-
-export default  function getTask(req, res) {
-  res.status(200).json("tasks")
-
-  // if(req.method === 'GET'){
-  //   const tasks = await TaskModel.find()
-  //   tasks? res.status(200).json(tasks) : res.status(404).json({message: 'No tasks found'})
-   
-
-  // }
-  
+    switch(method){
+        case 'GET':
+            try{
+                const allTasks = await TasksModel.find()
+                res.status(200).json(allTasks)
+                console.log(allTasks)
+            }catch(err){
+                res.status(500).json({message: err.message})
+            }
+            break
+        case 'POST':
+            try{
+                const newTask = await new TasksModel(body)
+                const savedTask = await newTask.save()
+                res.status(201).json(savedTask)
+            }catch(err){
+                res.status(500).json({message: err.message})
+            }
+            break
+        default:
+            res.status(400).json({message: 'Method not allowed'})
+    }
+    
+    
 }
