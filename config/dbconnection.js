60d940e967d74033be21fb837e19003bfbdeb2c1
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-03 14:41:13 -0300

    mongoose config

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,24 +1,49 @@
 require('dotenv').config();
-const mongoose = require('mongoose')
-const {
+import { connect, connection } from 'mongoose';
+// const {
     
-    URL_MONGODB
+//     URL_MONGODB
 
-  } = process.env;
+//   } = process.env;
+const uri = "mongodb+srv://todo_app:AA3E6JcQIpn6CFg0@cluster0.hc8pi.mongodb.net/task?retryWrites=true&w=majority";
 
 
 
-const connection = () =>{
-    mongoose.connect(URL_MONGODB, { useNewUrlParser: true, useUnifiedTopology:true})
-   
+export async function  dbConnect(){
+    const db = await connect (uri)
+   // console.log(db.connection[0].readyState)
 }
 
-mongoose.connection.on('open', _=>{
-console.log("se conecto la base de datos")
+
+
+
+// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// client.connect()
+//     .then(() => {
+//         console.log('start');
+//     })
+//     .catch(err => {
+//         console.error('App starting error:', err.stack);
+//         process.exit(1)
+//     });
+
+
+
+// mongoose.connect(, {
+//      useNewUrlParser: true, 
+//      useUnifiedTopology:true
+    
+//     })
+   
+
+
+connection.on('open', _=>{
+console.log("MongoDB is Connected")
 })
 
-mongoose.connection.on('error', error =>{
+connection.on('error', error =>{
   console.log(error)
 })
-
-connection()
