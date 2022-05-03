5eda2b4b90793acd2a42053b6f3f47ad670a2777
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-03 14:43:01 -0300

    add routes api

--- pages/api/tasks/list.js
+++ pages/api/tasks/list.js
@@ -1,9 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { tasks } from './_tasks'
+import { dbConnect } from '../../../config/dbconnection'
 
-console.log(tasks)
+dbConnect()
 
-export default (req, res) => {
-  res.status(200).json(tasks)
+import TaskModel from '../../../config/model'
+
+//import { tasks } from './_tasks'
+
+
+
+export default  function getTask(req, res) {
+  res.status(200).json("tasks")
+
+  // if(req.method === 'GET'){
+  //   const tasks = await TaskModel.find()
+  //   tasks? res.status(200).json(tasks) : res.status(404).json({message: 'No tasks found'})
+   
+
+  // }
+  
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import TaskForm from '../components/TaskForm'
 import styles from '../styles/main.module.css'
 
 export default function Home() {
@@ -14,41 +15,10 @@ export default function Home() {
           Welcome to Task Manager
         </h1>
 
-        <p className={styles.description}>
-          This is based in: <a href="https://nextjs.org">Next.js!</a><br/>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+       <section>
+        <TaskForm/>
+       </section>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
     </div>
   )
