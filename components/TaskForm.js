f4c4e557cdc6c0670cdbcc96f5f7bf7414d23d91
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-04 17:46:25 -0300

    create component for tasks

--- components/TaskForm.js
+++ components/TaskForm.js
@@ -1,79 +1,106 @@
-import axios from 'axios';
-import { useState } from 'react';
+import axios from "axios";
+import { useState } from "react";
+import {useRouter} from 'next/router'
 
- export default  function TaskForm(){
+export default function TaskForm() {
 
-const [input, setInput] = useState({
-    title: '',
-    description: '',
-    color: '',
-    status: 'Pending'
-})
+    const router = useRouter()
+  
+    const [input, setInput] = useState({
+    title: "",
+    description: "",
+    color: "",
+    status: "Pending",
+  });
 
-const colors = ["🔴", "🟡", "🟢", "🔵"];
+  const colors = ["🔴", "🟡", "🟢", "🔵"];
 
-const handleChangeInput = (e) => {
+  const handleChangeInput = (e) => {
     setInput({
-        ...input,
-        [e.target.name]: e.target.value
-    })
-}
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-const handleSelectColor = (e) => {
+  const handleSelectColor = (e) => {
     setInput({
-        ...input,
-        color: e.target.value
-    })
-}
+      ...input,
+      color: e.target.value,
+    });
+  };
 
-const handleSelectStatus = (e) => {
+  const handleSelectStatus = (e) => {
     setInput({
-        ...input,
-        status: e.target.value
-    })
-}
+      ...input,
+      status: e.target.value,
+    });
+  };
 
-
-const  handleSubmit = async (e) => {
-    e.preventDefault()
-    const response = await axios.post('api/tasks/list', {
-        title: input.title,
-        description: input.description,
-        status: input.status,
-        color: input.color
-    })
-    console.log(response)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("api/tasks/list", {
+      title: input.title,
+      description: input.description,
+      status: input.status,
+      color: input.color,
+    });
+    console.log(response);
     setInput({
-        title: '',
-        description: '',
-        color: '',
-        status: 'Pending'
-    })
-}
-    return(
-        <div>
-            <h1>Task Form</h1>
+      title: "",
+      description: "",
+      color: "",
+      status: "Pending",
+    });
+    router.push('/tasks/home')
 
-            <form onSubmit={handleSubmit}>
-                <label>Title</label>
-                <input type="text" name="title"  value={input.title} placeholder="Title" onChange={handleChangeInput}/>
-                <label>Description</label>
-                <textarea  name="description" value={input.description} placeholder="Description" rows="2" onChange={handleChangeInput}/>
-                <label>Color</label>
-                <select type="text" name="color" placeholder="Color" onClick={handleSelectColor}>
-                {colors.map((color,index) =>(
-                    <option key={index} value={color}>{color}</option>
-                ))}
-                </select>
-                <label>Status</label>
-                <select type="text" name="status" placeholder="Status" onClick={handleSelectStatus}>
-                <option value="Pending">Pending</option>
-                <option value="In Progress">In Progress</option>
-                <option value="Done">Done</option>
-                </select>
-                <button type="submit">Add Task</button>
-            </form>
+  };
+  return (
+    <div>
+      <h1>Task Form</h1>
 
-       </div>
-    )
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input
+          type="text"
+          name="title"
+          value={input.title}
+          placeholder="Title"
+          onChange={handleChangeInput}
+        />
+        <label>Description</label>
+        <textarea
+          name="description"
+          value={input.description}
+          placeholder="Description"
+          rows="2"
+          onChange={handleChangeInput}
+        />
+        <label>Color</label>
+        <select
+          type="text"
+          name="color"
+          placeholder="Color"
+          onClick={handleSelectColor}
+        >
+          {colors.map((color, index) => (
+            <option key={index} value={color}>
+              {color}
+            </option>
+          ))}
+        </select>
+        <label>Status</label>
+        <select
+          type="text"
+          name="status"
+          placeholder="Status"
+          onClick={handleSelectStatus}
+        >
+          <option value="Pending">Pending</option>
+          <option value="In Progress">In Progress</option>
+          <option value="Done">Done</option>
+        </select>
+        <button type="submit">Add Task</button>
+      </form>
+    </div>
+  );
 }
--- components/TaskList.js
+++ components/TaskList.js
@@ -1,10 +1,115 @@
+import * as React from 'react';
+import {useRouter} from 'next/router'
+import TablePaginationAction from './tools/TablePaginationAction';
+import PropTypes from 'prop-types';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+//icons
+import EditIcon from '@mui/icons-material/Edit';
+import ClearIcon from '@mui/icons-material/Clear';
+//import search from '../components/tools/Img/notcontent.jpg'
+//grid
+import Grid from '@mui/material/Grid';
 
 
 
-export default function TaskList (){
-    return(
-        <div>
-            <h1>Task List</h1>
-        </div>
-    )
+export default function TaskList ({data}){
+    //console.log("props de taskList",data)
+    const router = useRouter()
+
+    TablePaginationAction.propTypes = {
+        count: PropTypes.number.isRequired,
+        onPageChange: PropTypes.func.isRequired,
+        page: PropTypes.number.isRequired,
+        rowsPerPage: PropTypes.number.isRequired,
+      };
+
+      const [page, setPage] = React.useState(0);
+      const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    
+      // Avoid a layout jump when reaching the last page with empty rows.
+      const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
+    
+      const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+      };
+    
+      const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+      };
+
+      if(data.length === 0){
+        return (
+            <Grid  columns={1} style={{height: "80vh"}}>
+                <Grid item style={{textAlign:"center"}}>
+            <div>There are no tasks yet</div>
+            {/* <img src={search} alt="img"/> */}
+            <Button variant="contained" onClick={() => router.push('/task/create')}>New Task</Button>
+                </Grid>
+            </Grid>
+        
+        
+        )
+      }
+
+return(
+    <TableContainer component={Paper}>
+    <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+      <TableBody>
+        {(rowsPerPage > 0
+          ? data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+          : data
+        ).map((task) => (
+          <TableRow key={task._id}>
+            <TableCell component="th" scope="row">
+              {task.title}
+            </TableCell>
+            <TableCell style={{ width: 80 }} align="right">
+              <EditIcon />
+            </TableCell>
+            <TableCell style={{ width: 80 }} align="right">
+              <ClearIcon/>
+            </TableCell>
+          </TableRow>
+        ))}
+
+        {emptyRows > 0 && (
+          <TableRow style={{ height: 53 * emptyRows }}>
+            <TableCell colSpan={6} />
+          </TableRow>
+        )}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+            colSpan={3}
+            count={data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            SelectProps={{
+              inputProps: {
+                'aria-label': 'rows per page',
+              },
+              native: true,
+            }}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            ActionsComponent={TablePaginationAction}
+          />
+        </TableRow>
+      </TableFooter>
+    </Table>
+  </TableContainer>
+)
+    
 }
