bbaf86107f65c6dd6f95af7b94d16c5ca75e16d4
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-05 18:03:02 -0300

    add buttons delete and edit task

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,5 +1,6 @@
-import * as React from 'react';
+import { useEffect, useState } from 'react';
 import {useRouter} from 'next/router'
+import ButtonDelete from './ButtonDelete';
 import TablePaginationAction from './tools/TablePaginationAction';
 import PropTypes from 'prop-types';
 import Table from '@mui/material/Table';
@@ -22,7 +23,10 @@ import Grid from '@mui/material/Grid';
 
 export default function TaskList ({data}){
     //console.log("props de taskList",data)
-    const router = useRouter()
+
+    const {push} = useRouter()
+
+    const [open, setOpen] = useState(false);
 
     TablePaginationAction.propTypes = {
         count: PropTypes.number.isRequired,
@@ -31,8 +35,8 @@ export default function TaskList ({data}){
         rowsPerPage: PropTypes.number.isRequired,
       };
 
-      const [page, setPage] = React.useState(0);
-      const [rowsPerPage, setRowsPerPage] = React.useState(5);
+      const [page, setPage] = useState(0);
+      const [rowsPerPage, setRowsPerPage] = useState(5);
     
       // Avoid a layout jump when reaching the last page with empty rows.
       const emptyRows =
@@ -47,19 +51,34 @@ export default function TaskList ({data}){
         setPage(0);
       };
 
+//CONFIG DELETE BUTTON
+
+const handleClickOpen = () => {
+  setOpen(true);
+};
+
+const handleClose = () => {
+  setOpen(false);
+};
+
+      
+
+      //if not exits tasks
       if(data.length === 0){
         return (
             <Grid  columns={1} style={{height: "80vh"}}>
                 <Grid item style={{textAlign:"center"}}>
             <div>There are no tasks yet</div>
             {/* <img src={search} alt="img"/> */}
-            <Button variant="contained" onClick={() => router.push('/task/create')}>New Task</Button>
+            <Button variant="contained" onClick={() => push('/task/create')}>New Task</Button>
                 </Grid>
             </Grid>
         
         
         )
       }
+
+      //if exits tasks
 
 return(
     <TableContainer component={Paper}>
@@ -74,10 +93,16 @@ return(
               {task.title}
             </TableCell>
             <TableCell style={{ width: 80 }} align="right">
-              <EditIcon />
+             <Button onClick={()=> push(`/task/${task._id}`)}> <EditIcon /></Button>
             </TableCell>
             <TableCell style={{ width: 80 }} align="right">
-              <ClearIcon/>
+             <Button 
+            //  onClick={()=>handleDelete(task._id)}
+            onClick={handleClickOpen}
+             > 
+             <ClearIcon/> 
+             </Button>
+             <ButtonDelete open={open} handleClose={handleClose} id={task._id}/>
             </TableCell>
           </TableRow>
         ))}
