cb6c5d825c71e654b23c542204aed7a7475085f2
Karen Derkach <DERKACH.M.KAREN@GMAIL.COM>
2022-05-09 19:00:09 -0300

    add component Login

---
+++ components/Login.js
@@ -0,0 +1,133 @@
+import {useState} from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useRouter } from "next/router";
+
+
+const theme = createTheme();
+
+export default function Login() {
+  const router = useRouter();
+
+  const [error, setError] = useState('');
+
+  const [user, setUser] =useState({
+    email: '',
+    password: '',
+  })
+
+  const handleChange = (event) => {
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+      const user =await fetch('http://localhost:3000/api/auth/singIn', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+        , body: JSON.stringify(user)
+      })
+      if(user.status === 200){
+        router.push('/task/home')
+      }else{
+        setError('Invalid email or password')
+      }
+    
+    
+    
+      
+    
+  };
+
+
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              value={user.email}
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              value={user.password}
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            {error &&<Stack sx={{ width: '100%' }} spacing={2}>
+            <Alert severity="error"> {error}</Alert>
+            </Stack>}
+            <Grid container>
+              <Grid item>
+                <Link href="/singUp" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+      </Container>
+    </ThemeProvider>
+  );
+}
